535c8a9e4f2971743c990028883fccdf5734c335
DhavalCoder <DhavalCoder@users.noreply.github.com>
2026-08-12 15:21:21 +0530

    auto: saved changes

--- Python_001/threads_concurrency/threading.py
+++ Python_001/threads_concurrency/threading.py
@@ -8,3 +8,15 @@ def take_orders():
 
 
 
+def brew_chai():
+    for i in range(1,4):
+            print(f"Brewing chai for #{i}")
+            time.sleep(2)
+
+
+# Creating Threads
+order_thread = threading.Thread(target=take_orders)
+brew_thread = threading.Thread(target=brew_chai)
+
+order_thread.start()
+brew_thread.start()
