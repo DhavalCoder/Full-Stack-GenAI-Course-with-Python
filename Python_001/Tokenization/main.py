ef38dc8ebd0f42f59b6dc15aad76529319e93f14
DhavalCoder <DhavalCoder@users.noreply.github.com>
2026-08-16 17:05:02 +0530

    auto: saved changes

--- Python_001/Tokenization/main.py
+++ Python_001/Tokenization/main.py
@@ -3,6 +3,6 @@ import tiktoken
 
 enc = tiktoken.encoding_for_model("gpt-4o")
 
-text = "My Name is Dhava Chorwadkar"
-encode = enc.text
-print("Tokens",  encode)
+text = "My Name is Dhaval Chorwadkar"
+tokens = enc.encode(tokens) 
+print("Tokens",  tokens)
