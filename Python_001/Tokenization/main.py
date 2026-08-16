7bcd4490bc5441a9a291e22e95caaa08760d0511
DhavalCoder <DhavalCoder@users.noreply.github.com>
2026-08-16 16:52:39 +0530

    auto: saved changes

--- Python_001/Tokenization/main.py
+++ Python_001/Tokenization/main.py
@@ -6,3 +6,6 @@ text = "Hey There! My name is Dhaval Chorwadkar"
 tokens = enc.encode(text)
 
 print("Tokens", tokens)
+
+decoded = enc.decode([25216, 3274, 0, 3673, 1308, 382, 31129, 24733, 125322, 40645, 10428])
+print("Decoded", decoded)
