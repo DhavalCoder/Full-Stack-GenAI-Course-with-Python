25595c17485485b7ca68437eed614c120f830203
DhavalCoder <DhavalCoder@users.noreply.github.com>
2026-08-26 19:23:56 +0530

    auto: saved changes

---
+++ Gen-AI/rag_queue/queues/server.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI, Query
+from ..clients.rq_client import queue
+from ..queues.worker import process_query
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"status": "Server is up and running"}
+
+
+@app.post("/chat")
+def chat(
+    query: str = Query(..., description="The chat query of user")
+):
+    job = queue.enqueue(process_query, query)
+
+    return {
+        "status": "queued",
+        "job_id": job.id
+    }
+
+
+@app.get("/job-status")
+def get_result(
+    job_id: str = Query(..., description="Job ID")
+):
+    job = queue.fetch_job(job_id)
+
+    if job is None:
+        return {"error": "Job not found"}
+
+    result = job.return_value
+
+    return {
+        "result": result
+    }
